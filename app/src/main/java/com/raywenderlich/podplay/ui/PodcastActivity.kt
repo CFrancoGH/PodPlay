51120b62b8c3caed12511c45815f710a87a1d766
email <email.cfranco@gmail.com>
2021-09-23 15:35:03 -0400

    9/23 3:34pm

--- app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -1,9 +1,17 @@
 package com.raywenderlich.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+
+
 import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.databinding.ActivityPodcastBinding
 import com.raywenderlich.podplay.repository.ItunesRepo
 import com.raywenderlich.podplay.service.ItunesService
 import kotlinx.coroutines.GlobalScope
@@ -11,11 +19,15 @@ import kotlinx.coroutines.launch
 
 class PodcastActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPodcastBinding
+
     private val TAG = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityPodcastBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val itunesService = ItunesService.instance
         val itunesRepo = ItunesRepo(itunesService)
@@ -24,5 +36,55 @@ class PodcastActivity : AppCompatActivity() {
             val results = itunesRepo.searchByTerm("Android Developer")
             Log.i(TAG, "Results = ${results.body()}")
         }
+
+        setupToolbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        //1
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        //2
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        //3
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        //4
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
+    }
+
+    private fun performSearch(term: String) {
+
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm(term)
+            Log.i(TAG, "Results = ${results.body()}")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if(Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?:
+            return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
     }
 }
